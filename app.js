9360840672a903c870427085d80a072f0dd3001d
Jaden Loh <133660578+lohlifer@users.noreply.github.com>
2024-02-16 18:31:21 +0800

    updated api

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const port = 3000;
 
-const app = express()
-const uri = ""
+const app = express();
+const uri =
+  "mongodb+srv://virtuTest1:YGV4DecXxhFdyMdu@virtuta.884jcds.mongodb.net?retryWrites=true&w=majority";
 
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'virtuTA'
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
+    dbName: "virtuTA",
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 app.use(express.json());
 
-app.use('/users', require('./model/user.js'));
-app.use('/documents', require('./model/document.js'));
+app.use("/query", require("./controllers/query-controller.js"));
+app.use("/documents", require("./controllers/document-controller.js"));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
